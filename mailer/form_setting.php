c8acf15a5ec0bce3493dea9ebe07007b29ee232e
Tejas Sangle <79887775+Crazyscorpion26@users.noreply.github.com>
2023-08-14 19:14:08 +0530

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Tejas's portfolio websites'"; //letter subject
+	$to = 'sangletejas1@gmail.com'; // Enter Your E-mail
+	$from = 'sangletejas1@gmail.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
